b85f5557bd9d390cea51e93bb37ab992f95e5c57
Isaac Rahel Martim Oliveira <rahelund@MacBook-Air-de-Isaac.local>
2022-03-25 10:39:07 -0300

    test with stryker

--- SimpleMath/SimpleMath.cs
+++ SimpleMath/SimpleMath.cs
@@ -1,6 +1,16 @@
 ﻿namespace SimpleMath;
 public class SimpleMathClass
 {
-    public static int Add(int number1, int number2) => number1 + number2;
+    public static int Add(int number1, int number2)
+    {
+        if (number1 >= number2)
+        {
+            return number1 + number2;
+        }
+        else
+        {
+            return number2 + number1;
+        }
+    }
     public static int Subtract(int number1, int number2) => number1 - number2; 
 }
--- SimpleMathTest/UnitTest1.cs
+++ SimpleMathTest/UnitTest1.cs
@@ -11,18 +11,27 @@ public class SimpleMathClassTest
     [Trait("Category","Sum")]
     public static void TestAdd()
     {
-        int sum = SimpleMathClass.Add(10, 10);
-        sum.Should().Be(20);                
+        int sum = SimpleMathClass.Add(10, 5);
+        sum.Should().Be(15);                
+    }
+
+    [Fact(DisplayName = "testing a single addition2")]
+    [Trait("Category","Sum")]
+    public static void TestAdd2()
+    {
+        int sum = SimpleMathClass.Add(5, 40);
+        sum.Should().Be(45);                
     }
     
     [Theory(DisplayName = "Testing multiple subtractions")]
     [Trait("Category","Sub")]
-    [InlineData(10,10,20)]
-    [InlineData(15,15,30)]
-    [InlineData(20,20,40)]
+    [InlineData(10,10,0)]
+    [InlineData(15,10,5)]
+    [InlineData(20,10,10)]
     public static void TestSubtract(int n1, int n2, int expected)
     {
         int sub = SimpleMathClass.Subtract(n1, n2);        
         sub.Should().Be(expected); 
     }
+    
 }
